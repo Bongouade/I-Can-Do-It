1b0ee8bb22c2fdc49f7823405f952c062acf1273
BRICE DAVID BONGOUADE <bongouade@gmail.com>
2020-09-02 01:07:45 +0000

    add model

---
+++ lib/models/challenge_model.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+
+enum unity_challenge {
+  kg,
+  km,
+}
+
+class ChallengeModel {
+  final String name;
+  final int target;
+  final unity_challenge unity;
+
+  ChallengeModel({
+    @required this.name,
+    @required this.target,
+    @required this.unity,
+  });
+
+  ChallengeModel.fromJSON(Map<String, dynamic> json)
+      : name = json['name'],
+        target = json['target'],
+        unity = json['unity'];
+
+  Map<String, dynamic> toJSON() {
+    return {
+      "anme": name,
+      "target": target,
+      "unity": unity.toString(),
+    };
+  }
+}

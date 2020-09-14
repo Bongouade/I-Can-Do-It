f986b5492bec77817af666a48d2bd7ddd8761da4
BRICE DAVID BONGOUADE <bongouade@gmail.com>
2020-09-14 19:09:01 +0000

    add form

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,13 +6,18 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  String unityChallenge = "KG";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
       appBar: AppBar(
         title: Text("ICanDoIt"),
+        centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0.0,
       ),
@@ -33,7 +38,77 @@ class _HomeState extends State<Home> {
       return Container(
         alignment: Alignment.center,
         height: MediaQuery.of(context).size.height * 0.5,
-        child: Text("hello"),
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ListView(
+              children: [
+                TextFormField(
+                  validator: (value) {
+                    final RegExp checkReg = RegExp(r'^\D+$');
+                    if (value.isEmpty) {
+                      return "Merci d'entrer un nom pour le challenge";
+                    } else if (!checkReg.hasMatch(value)) {
+                      return "$value";
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Nom du Challenge",
+                  ),
+                ),
+                TextFormField(
+                  validator: (value) {
+                    final _isInt = int.tryParse(value);
+                    if (_isInt == null) {
+                      return "Merci d'entrer uniquement des chiffres pour l'ojectif";
+                    }
+
+                    return null;
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Objectif",
+                  ),
+                ),
+                DropdownButtonFormField(
+                  value: unityChallenge,
+                  onChanged: (value) {
+                    setState(() {
+                      unityChallenge = value;
+                      //  print(value);
+                    });
+                  },
+                  onSaved: (value) {
+                    setState(() {
+                      unityChallenge = value;
+                      // print("2");
+                    });
+                  },
+                  items: <DropdownMenuItem>[
+                    DropdownMenuItem(
+                      value: "KG",
+                      child: Text("Kg"),
+                    ),
+                    DropdownMenuItem(
+                      value: "Km",
+                      child: Text("Km"),
+                    ),
+                  ],
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    if (formKey.currentState.validate()) {
+                      // formKey.currentState.save();
+                    }
+                  },
+                  child: Text("Ajouter le Chanllenge"),
+                )
+              ],
+            ),
+          ),
+        ),
       );
     });
   }

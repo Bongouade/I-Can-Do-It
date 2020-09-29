f3d98e34b31dff86ad0a04165d30c7e6e6ec6779
BRICE DAVID BONGOUADE <bongouade@gmail.com>
2020-09-29 20:22:28 +0000

    refactorisation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,106 +30,106 @@ class _HomeState extends State<Home> {
       body: ChallengesListBuilder(),
       backgroundColor: Color(0xff414a4c),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.orange[700],
-        onPressed: () {
-          buildBottomSheet();
-        },
-        child: Icon(Icons.add),
-      ),
+      floatingActionButton: buildBottomSheet(),
     );
   }
 
-  PersistentBottomSheetController buildBottomSheet() {
-    return scaffoldKey.currentState.showBottomSheet((context) {
-      return Container(
-        alignment: Alignment.center,
-        height: MediaQuery.of(context).size.height * 0.5,
-        child: Form(
-          key: formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: ListView(
-              children: [
-                TextFormField(
-                  onSaved: (value) {
-                    nameChallenge = value;
-                  },
-                  validator: (value) {
-                    final RegExp checkReg = RegExp(r'^\D+$');
-                    if (value.isEmpty) {
-                      return "Merci d'entrer un nom pour le challenge";
-                    } else if (!checkReg.hasMatch(value)) {
-                      return "$value";
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                    labelText: "Nom du Challenge",
+  FloatingActionButton buildBottomSheet() {
+    return FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.orange[700],
+        onPressed: () {
+          scaffoldKey.currentState.showBottomSheet((context) {
+            return Container(
+              alignment: Alignment.center,
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: Form(
+                key: formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ListView(
+                    children: [
+                      TextFormField(
+                        onSaved: (value) {
+                          nameChallenge = value;
+                        },
+                        validator: (value) {
+                          final RegExp checkReg = RegExp(r'^\D+$');
+                          if (value.isEmpty) {
+                            return "Merci d'entrer un nom pour le challenge";
+                          } else if (!checkReg.hasMatch(value)) {
+                            return "$value";
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: "Nom du Challenge",
+                        ),
+                      ),
+                      TextFormField(
+                        onSaved: (value) {
+                          targetChallenge = value;
+                        },
+                        validator: (value) {
+                          final _isInt = int.tryParse(value);
+                          if (_isInt == null) {
+                            return "Merci d'entrer uniquement des chiffres pour l'ojectif";
+                          }
+
+                          return null;
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: "Objectif",
+                        ),
+                      ),
+                      DropdownButtonFormField(
+                        value: unityChallenge,
+                        onChanged: (value) {
+                          setState(() {
+                            unityChallenge = value;
+                            //  print(value);
+                          });
+                        },
+                        onSaved: (value) {
+                          setState(() {
+                            unityChallenge = value;
+                            // print("2");
+                          });
+                        },
+                        items: <DropdownMenuItem>[
+                          DropdownMenuItem(
+                            value: "KG",
+                            child: Text("Kg"),
+                          ),
+                          DropdownMenuItem(
+                            value: "KM",
+                            child: Text("Km"),
+                          ),
+                        ],
+                      ),
+                      RaisedButton(
+                        onPressed: () {
+                          if (formKey.currentState.validate()) {
+                            formKey.currentState.save();
+
+                            Provider.of<ChallengesController>(context)
+                                .addChallenge(
+                                    name: nameChallenge,
+                                    target: targetChallenge,
+                                    unity: unityChallenge);
+
+                            Navigator.pop(context);
+                          }
+                        },
+                        child: Text("Ajouter le Chanllenge"),
+                      )
+                    ],
                   ),
                 ),
-                TextFormField(
-                  onSaved: (value) {
-                    targetChallenge = value;
-                  },
-                  validator: (value) {
-                    final _isInt = int.tryParse(value);
-                    if (_isInt == null) {
-                      return "Merci d'entrer uniquement des chiffres pour l'ojectif";
-                    }
-
-                    return null;
-                  },
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText: "Objectif",
-                  ),
-                ),
-                DropdownButtonFormField(
-                  value: unityChallenge,
-                  onChanged: (value) {
-                    setState(() {
-                      unityChallenge = value;
-                      //  print(value);
-                    });
-                  },
-                  onSaved: (value) {
-                    setState(() {
-                      unityChallenge = value;
-                      // print("2");
-                    });
-                  },
-                  items: <DropdownMenuItem>[
-                    DropdownMenuItem(
-                      value: "KG",
-                      child: Text("Kg"),
-                    ),
-                    DropdownMenuItem(
-                      value: "KM",
-                      child: Text("Km"),
-                    ),
-                  ],
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    if (formKey.currentState.validate()) {
-                      formKey.currentState.save();
-
-                      Provider.of<ChallengesController>(context).addChallenge(
-                          name: nameChallenge,
-                          target: targetChallenge,
-                          unity: unityChallenge);
-
-                      Navigator.pop(context);
-                    }
-                  },
-                  child: Text("Ajouter le Chanllenge"),
-                )
-              ],
-            ),
-          ),
-        ),
-      );
-    });
+              ),
+            );
+          });
+        });
   }
 }
